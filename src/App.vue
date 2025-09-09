2e01132160d42210bf9a430dc2483d7e065a5ed7
hwsanchez <hwsanchez.poreda@gmail.com>
2025-09-09 09:23:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Child from './components/Child.vue'
+import { ref } from 'vue';
+
+const title = ref('Star Wars');
+const author = ref('George Lucas');
+</script>
+
+<template>
+<h1>{{ title }}</h1>
+<p>{{ author}} </p>
+
+<Child v-model:titulo="title" v-model:author="author"></Child>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,23 @@
+<script setup>
+const titulo= defineModel('titulo', { required: true});
+const author = defineModel('author', { required: true});
+
+
+</script>
+
+<template>
+<div>
+<label for="titulo">Titulo: </label>
+<input id="titulo" type="text" v-model="titulo"/>
+</div>
+ <div> 
+ <label for="autor">Autor: </label>
+ <input id="autor" type="text" v-model="author"/>
+</div>
+
+
+</template>
+
+<style scoped>
+
+</style>
